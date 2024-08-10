b9c3a0266726c9aa98329e4a4e8f63b046b11202
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-10 15:43:23 +0300

    Add files via upload

---
+++ PatikaOOP3/PatikaOOP3/Library.cs
@@ -0,0 +1,73 @@
+﻿public class PatikaLibrary
+{
+    private string _bookName;
+    private string _authorName;
+    private string _authorSurname;
+    private int _pageNumber;
+    private string _puslishingHouse;
+    private DateTime _recordDate;
+
+    public PatikaLibrary() { _recordDate = DateTime.Now; }
+
+    public PatikaLibrary(string bookName, string authorName, string authorSurname, string publishingHouse, int pageNumber)
+    {
+        _bookName = bookName;
+        _authorName = authorName;
+        _authorSurname = authorSurname;
+        _pageNumber = pageNumber;
+        _puslishingHouse = publishingHouse;
+        _recordDate = DateTime.Now;  
+
+    }
+    public string BookName
+    {
+        get { return _bookName; }
+        set { _bookName = value; }
+    }
+
+    public string AuthorName
+    {
+        get { return _authorName; }
+        set { _authorName = value; }
+    }
+
+    public string AuthorSurname
+    {
+        get { return _authorSurname; }
+        set { _authorSurname = value; }
+    }
+
+    public int PageNumber
+    {
+        get { return _pageNumber; }
+        set
+        {
+            if (value > 0)
+                _pageNumber = value;
+            else
+                Console.WriteLine("Geçerli sayafa sayısı girilmedi.");
+        }
+    }
+    public string PublishingHouse
+    {
+        get { return _puslishingHouse; }
+        set { _puslishingHouse = value; }
+    }
+
+    public DateTime RecordDate
+    {
+        get { return _recordDate; }
+        set { _recordDate = value; }
+    }
+
+    public void RecordingInfo()
+    {
+        Console.WriteLine($"{_bookName} isimli kitap, {_recordDate} tarihinde kitapliığa eklenmiştir.");
+    }
+
+}
+
+// Class : Verileri ve bu veriler üzerinde işlem yapan metotları kapsayan, nesne oluşturma şablonudur. 
+// Property: Sınıfın veri üyelerine erişim sağlar.
+// New : Sınıf üzerinden yeni bir nesne oluşturmak için kullanılır. 
+// Constructor : Oluşturulan nesnenin başlangıç değerlerini ayarlamak için otomatik olarak çağrılan metottur. 
---
+++ PatikaOOP3/PatikaOOP3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        PatikaLibrary book1 = new PatikaLibrary("Adı:Aylin", "Ayşe", "Kulin", "Remzi Kitabevi",398);
+        book1.RecordingInfo();
+
+        PatikaLibrary book2 = new PatikaLibrary();
+        book2.BookName = "Başka Bir Kitap";
+        book2.AuthorName = "Ayşe";
+        book2.AuthorSurname = "Karann";
+        book2.PublishingHouse = "Beta Yayıncılık";
+        book2.PageNumber = 215;
+        book2.RecordingInfo();
+
+        PatikaLibrary book3 = new PatikaLibrary("Son Kitap","Elif","Şahin","Delta Yayıncılık", 275);
+        book3.RecordingInfo();
+    }
+}
